484e877fa8f79b0bd581d20fc5f8327d47b93716
Mikko Syrjä <mikko@syrja.org>
2018-03-17 00:14:26 +0200

    Remove old Meego keyboard functions.

--- src/manager.cpp
+++ src/manager.cpp
@@ -2,9 +2,6 @@
 #include <QFile>
 #include <QDir>
 #include <QGuiApplication>
-#if !defined(Q_WS_SIMULATOR) && !defined(SAILFISH)
-#include <MGConfItem>
-#endif
 #include "core/functions.h"
 #include "core/constants.h"
 
@@ -59,150 +56,6 @@ QString Manager::calculate(const QString &input)
 }
 
 //
-void Manager::loadLayouts()
-{
-#if !defined(Q_WS_SIMULATOR) && !defined(SAILFISH)
-		//constants
-		QString settingsPath = QDir::homePath() + QDir::separator() + ".mobileCrunch" + QDir::separator();
-
-		//load gconf to memory
-		MGConfItem configuredLayoutsActive("/meegotouch/inputmethods/onscreen/active");
-		MGConfItem configuredLayoutsEnabled("/meegotouch/inputmethods/onscreen/enabled");
-
-		//save old config
-		QStringList availableLayouts;
-		if (!(QDir(settingsPath).exists())) QDir().mkpath(settingsPath);
-		//enabled
-		qDebug()<<"saving available layouts to file";
-
-		QVariant layouts = configuredLayoutsEnabled.value();
-		if (layouts.type() == QVariant::StringList) availableLayouts = layouts.toStringList();
-		if (availableLayouts.contains("mobileCrunchVkb_1.xml")) return; //if mobilecrunch layouts are already loaded abort
-		QFile file(settingsPath + "oldEnabledKb");
-		//file.open(QIODevice::Truncate); file.close();
-		file.open(QIODevice::WriteOnly);
-		QString old ="";
-		QTextStream out2(&file);
-		for (int i=0; i<availableLayouts.size()-1; i+=2)
-		{
-			if (old!=availableLayouts.at(i+1))
-			{
-				out2 << availableLayouts.at(i) << "\n"; //bug in the system?? it duplicates the active layout
-				out2 << availableLayouts.at(i+1) << "\n"; //bug in the system?? it duplicates the active layout
-				qDebug()<<availableLayouts.at(i+1);
-			}
-			old=availableLayouts.at(i+1);
-		}
-		file.close();
-
-		//active
-		layouts.clear();
-		layouts = configuredLayoutsActive.value();
-		qDebug()<<"saving active layout to file";
-		//availableLayouts.empty();
-		if (layouts.type() == QVariant::StringList) availableLayouts = layouts.toStringList();
-		if (availableLayouts.contains("mobileCrunchVkb_1.xml")) return; //if mobilecrunch layouts are already loaded abort
-		file.setFileName(settingsPath + "oldActiveKb");
-		//file.open(QIODevice::Truncate); file.close();
-		file.open(QIODevice::WriteOnly | QIODevice::Text);
-		QTextStream out(&file);
-		foreach (QString layout, availableLayouts)
-		{
-			out << layout << "\n";
-			qDebug()<<layout;
-		}
-		file.close();
-
-
-		//start loading our layouts
-
-		QString binFilename(QString("libmeego-keyboard.so"));
-		QStringList mobileCrunchLayouts;
-		QStringList mobileCrunchActiveLayout;
-		mobileCrunchActiveLayout.empty();
-		mobileCrunchActiveLayout.append(binFilename);
-		mobileCrunchActiveLayout.append("mobileCrunchVkb_1.xml");
-		mobileCrunchLayouts.empty();
-
-		int i = 1;
-		do {
-			QString layout; layout.setNum(i);
-			QString layoutFilename(QString("mobileCrunchVkb_") + layout + QString(".xml"));
-			QString pathToCreate = QDir::homePath() + QDir::separator() + QString(".config/meego-keyboard/layouts/");
-
-			if (!(QDir(pathToCreate).exists()))
-			{
-				QDir().mkpath(pathToCreate);
-				qDebug() << "Created dir: " << pathToCreate; // Check the result
-			}
-
-			qDebug() << "Depositing: " << pathToCreate + layoutFilename;
-			QFile::remove(pathToCreate + layoutFilename);
-			QFile::copy("/opt/mobilecrunch/mobileCrunchVkb_"+layout+".xml",pathToCreate + layoutFilename);
-			mobileCrunchLayouts.append(binFilename);
-			qDebug()<<layoutFilename;
-			mobileCrunchLayouts.append(layoutFilename);
-			i++;
-		} while (QFile::exists("/opt/mobilecrunch/mobileCrunchVkb_"+QString::number(i)+".xml"));
-
-		configuredLayoutsEnabled.set(mobileCrunchLayouts);
-		configuredLayoutsActive.set(mobileCrunchActiveLayout);
-		//have to add additional layouts later to ensure active layout remains the above
-		mobileCrunchLayouts.append(binFilename);
-		mobileCrunchLayouts.append("en_us.xml");
-		configuredLayoutsEnabled.set(mobileCrunchLayouts);
-#endif
-}
-
-//
-void Manager::restoreLayouts()
-{
-#if !defined(Q_WS_SIMULATOR) && !defined(SAILFISH)
-	qDebug()<<"destroy";
-	//constants
-	QString settingsPath = QDir::homePath() + QDir::separator() + ".mobileCrunch" + QDir::separator();
-
-	//load gconf to memory
-	MGConfItem configuredLayoutsActive("/meegotouch/inputmethods/onscreen/active");
-	MGConfItem configuredLayoutsEnabled("/meegotouch/inputmethods/onscreen/enabled");
-
-	//load old config
-	//enabled
-	QStringList availableLayouts;
-	QFile file(settingsPath + "oldEnabledKb");
-	file.open(QIODevice::ReadOnly | QIODevice::Text);
-	QTextStream in(&file);
-	while (!in.atEnd())
-	{
-		//qDebug()<<"*";
-		availableLayouts.append(in.readLine());
-	}
-	file.close();
-
-	QStringList enabledLayouts;
-	QVariant layouts = configuredLayoutsEnabled.value();
-	if (layouts.type() == QVariant::StringList) enabledLayouts = layouts.toStringList();
-	if (!enabledLayouts.contains("mobileCrunchVkb_1.xml")) return; //do not restore false layouts
-
-
-	configuredLayoutsEnabled.set(availableLayouts);
-
-	//active
-	availableLayouts.empty();
-	file.setFileName(settingsPath + "oldActiveKb");
-	file.open(QIODevice::ReadOnly | QIODevice::Text);
-	availableLayouts.empty();
-	while (!in.atEnd()) {
-		//qDebug()<<"*";
-		availableLayouts.append(in.readLine());
-	}
-	file.close();
-	configuredLayoutsActive.set(availableLayouts);
-
-#endif
-}
-
-//
 QString Manager::getFunctions(QString filter)
 {
 	QStringList functions = Functions::instance()->names();
@@ -225,34 +78,6 @@ QString Manager::getFunctions(QString filter)
 	}
 
 	return result += "]";
-}
-
-//! Set letter keyboard.
-void Manager::setABC()
-{
-#if !defined(Q_WS_SIMULATOR) && !defined(SAILFISH)
-	//load gconf to memory
-	MGConfItem configuredLayoutsActive("/meegotouch/inputmethods/onscreen/active");
-	QString binFilename(QString("libmeego-keyboard.so"));
-	QStringList currentLayout;
-	currentLayout.append(binFilename);
-	currentLayout.append("en_us.xml");
-	configuredLayoutsActive.set(currentLayout);
-#endif
-}
-
-//! Set numeric keyboard.
-void Manager::setNumbers()
-{
-#if !defined(Q_WS_SIMULATOR) && !defined(SAILFISH)
-	//load gconf to memory
-	MGConfItem configuredLayoutsActive("/meegotouch/inputmethods/onscreen/active");
-	QString binFilename(QString("libmeego-keyboard.so"));
-	QStringList currentLayout;
-	currentLayout.append(binFilename);
-	currentLayout.append("mobileCrunchVkb_1.xml");
-	configuredLayoutsActive.set(currentLayout);
-#endif
 }
 
 //! Set angle unit.

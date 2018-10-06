f2a5458ec9decfc78f1b3b89a05e0ae017d06173
Mikko Syrjä <mikko@3d-system.fi>
2018-10-07 01:05:45 +0300

    Localizatin fixes.

--- lrelease.sh
+++ lrelease.sh
@@ -1,3 +1,4 @@
 lrelease -idbased locale/mobile.*.ts
 if [ $? -ne 0 ]; then
 	lrelease-qt5 -idbased locale/mobile.*.ts
+fi
--- lupdate.sh
+++ lupdate.sh
@@ -1,3 +1,4 @@
 lupdate -locations none qml -ts locale/mobile.*.ts
 if [ $? -ne 0 ]; then
 	lupdate-qt5 -locations none qml -ts locale/mobile.*.ts
+fi
